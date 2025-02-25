a45a3bb81e7ef8bd8cc4ac9bf5147144b3f10f57
zsuiko <zsuiko3211@gmail.com>
2025-02-25 16:06:48 +0100

    pizza model, controller created and prepared. pizza model added to dbcontext

--- PizzaBackend/Data/AppDbContext.cs
+++ PizzaBackend/Data/AppDbContext.cs
@@ -14,5 +14,6 @@ namespace PizzaBackend.Data
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
     }
 }
---
+++ PizzaBackend/Models/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PizzaBackend.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        public string PizzaName { get; set; } = string.Empty;
+        public string PizzaDescription { get; set; } = string.Empty;
+        public decimal PizzaPrice { get; set; }
+        public string PizzaImgUrl {  get; set; } = string.Empty;
+
+        public DateTime PizzaCreatedDate { get; set; } = DateTime.Now;
+
+    }
+}
