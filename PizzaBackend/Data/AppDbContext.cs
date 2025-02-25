09804a09983aec2ec895190c45dd6ab98f346d3f
zsuik <zsuik@DESKTOP-45HR3QB>
2025-02-25 15:26:44 +0100

    Add project files.

---
+++ PizzaBackend/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using PizzaBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+
+namespace PizzaBackend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ PizzaBackend/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PizzaBackend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public Role Role { get; set; } = Role.Customer;
+        public DateTime RegisteredDate { get; set; } = DateTime.Now;
+
+    }
+
+    public enum Role
+    {
+        
+        Admin = 0,// Admin - teljes jogosultság
+        Customer = 1// Customer - részleges jogosultság
+
+
+    }
+}
