e09c247495f80d1d07e269ed39e56438cc497305
Decay1979 <feilbence04@gmail.com>
2025-02-26 10:27:55 +0100

    cart model and controller created and preparied and dbcontext changed

---
+++ PizzaBackend/Models/Cart.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PizzaBackend.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int PizzaId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; } = 1;
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal UnitPrice { get; set; } // Az adott pizza ára a rendeléskor
+
+        [ForeignKey("OrderId")]
+        public Order? Order { get; set; }
+
+        [ForeignKey("PizzaId")]
+        public Pizza? Pizza { get; set; }
+    }
+}
